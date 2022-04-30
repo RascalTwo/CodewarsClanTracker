f679daeadfabbbb7f5e4402260c9a29749e112ad
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-30 18:11:50 -0400

    Fix index page static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export default function HomePage({ start }: { start: number }) {
 export async function getStaticProps() {
   return {
     props: {
-      start: +(await fs.promises.readdir(process.env.DAILY_CLAN_DIRECTORY!)).sort().slice(1)[0].split('.')[0],
+      start: +((await fs.promises.readdir(process.env.DAILY_CLAN_DIRECTORY!)).sort().slice(1)[0] || (Date.now().toString() + '.')).split('.')[0],
     },
   };
 }
