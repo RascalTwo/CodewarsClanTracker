e0a2a02f2f4776e9c960b977490d903162fd5344
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-24 03:43:30 -0400

    Fix calendar generation performance

--- pages/api/calendar.ts
+++ pages/api/calendar.ts
@@ -1,7 +1,9 @@
 import fs from 'fs';
+import path from 'path';
+
+import cacheData from 'memory-cache';
 
 import type { NextApiRequest, NextApiResponse } from 'next';
-import path from 'path';
 
 function getNearest(to: number, others: number[]) {
   let nearest = Infinity;
@@ -30,11 +32,13 @@ const readJSONFile = async (filepath: string) => {
   return JSON_CACHE[filepath];
 };
 
-const statsToUserMap = async (time: number): Promise<Record<string, UserClanInfo>> =>
-  (await readJSONFile(path.join(process.env.DAILY_CLAN_DIRECTORY!, time + '.json'))).reduce(
-    (map: any, user: any) => ({ ...map, [user.username]: user }),
-    {},
-  );
+const statsToUserMap = async (time: number): Promise<Record<string, UserClanInfo>> => {
+  let userMap: Record<string, UserClanInfo> = {};
+  for (const user of await readJSONFile(path.join(process.env.DAILY_CLAN_DIRECTORY!, time + '.json'))) {
+    userMap[user.username] = user;
+  }
+  return userMap;
+};
 
 const flattenDate = (input: any) => {
   const date = new Date(input);
@@ -57,57 +61,40 @@ const getUsersWithHonorChanges = async (start: number, end: number, times: numbe
 
 const trimUser = ({ username, honor, honorChange }: any) => ({ username, honor, honorChange });
 
-const CACHED_RESPONSE = fs.existsSync('./cache/calendar.json')
-  ? JSON.parse(fs.readFileSync('./cache/calendar.json').toString())
-  : { times: [], response: null, started: false };
-
 export async function generateNewCalendarData(times: number[]) {
-  console.log('Generating new calendar data...');
   const end = times.at(-1)!;
 
-  CACHED_RESPONSE.started = true;
-  try {
-    const months: Record<number, Top3> = {};
+  const months: Record<number, Top3> = {};
 
-    const monthStart = flattenDate(times[0]);
-    monthStart.setUTCDate(1);
-    let current = monthStart.getTime();
-    while (current < end) {
-      const nextMonth = new Date(current);
-      nextMonth.setUTCMonth(nextMonth.getUTCMonth() + 1);
-      months[current] = (await getUsersWithHonorChanges(current, nextMonth.getTime(), times)).map(trimUser);
-      current = nextMonth.getTime();
-    }
-
-    const weeks: Record<number, Top3> = {};
-
-    const weekStart = flattenDate(times[0]);
-    weekStart.setUTCDate(weekStart.getUTCDate() - weekStart.getUTCDay());
-    current = weekStart.getTime();
-    while (current < end) {
-      weeks[current] = (await getUsersWithHonorChanges(current, current + 86400000 * 7, times)).map(trimUser);
-      current += 86400000 * 7;
-    }
-
-    const days: Record<number, Top3> = {};
-    current = times[0];
-    while (current < end) {
-      days[current] = (await getUsersWithHonorChanges(current, current + 86400000, times)).map(trimUser);
-      current += 86400000;
-    }
-
-    const response = { days, months, weeks };
-    CACHED_RESPONSE.times = times;
-    CACHED_RESPONSE.response = response;
-    return response;
-  } catch (err) {
-    console.error(err);
-    throw err;
-  } finally {
-    CACHED_RESPONSE.started = false;
-    await fs.promises.writeFile('./cache/calendar.json', JSON.stringify(CACHED_RESPONSE, null, '  '));
-    console.log('New calendar data generated.');
+  const monthStart = flattenDate(times[0]);
+  monthStart.setUTCDate(1);
+  let current = monthStart.getTime();
+  while (current < end) {
+    const nextMonth = new Date(current);
+    nextMonth.setUTCMonth(nextMonth.getUTCMonth() + 1);
+    months[current] = (await getUsersWithHonorChanges(current, nextMonth.getTime(), times)).map(trimUser);
+    current = nextMonth.getTime();
   }
+
+  const weeks: Record<number, Top3> = {};
+
+  const weekStart = flattenDate(times[0]);
+  weekStart.setUTCDate(weekStart.getUTCDate() - weekStart.getUTCDay());
+  current = weekStart.getTime();
+  while (current < end) {
+    weeks[current] = (await getUsersWithHonorChanges(current, current + 86400000 * 7, times)).map(trimUser);
+    current += 86400000 * 7;
+  }
+
+  const days: Record<number, Top3> = {};
+  current = times[0];
+  while (current < end) {
+    days[current] = (await getUsersWithHonorChanges(current, current + 86400000, times)).map(trimUser);
+    current += 86400000;
+  }
+
+  const response = { days, months, weeks };
+  return response;
 }
 
 export const getDailyClanTimes = async () =>
@@ -117,22 +104,13 @@ export const getDailyClanTimes = async () =>
     .map(filename => +filename.split('.')[0]);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (CACHED_RESPONSE.started) {
-    if (CACHED_RESPONSE.response) return res.send(CACHED_RESPONSE.response);
-    console.log('Waiting for previous request to finish generating first calendar data...');
-    while (CACHED_RESPONSE.started) await new Promise(resolve => setTimeout(resolve, 5000));
-  }
-
   const times = await getDailyClanTimes();
 
-  if (JSON.stringify(times) === JSON.stringify(CACHED_RESPONSE.times)) return res.send(CACHED_RESPONSE.response);
+  const cacheKey = JSON.stringify(times);
+  const cached = cacheData.get(cacheKey);
+  if (cached) return res.send(cached);
 
-  const generator = generateNewCalendarData(times);
-
-  if (CACHED_RESPONSE.response) {
-    console.log('Using previous calendar data while requesting new calendar data...');
-    return res.send(CACHED_RESPONSE.response);
-  }
-  console.log('Generating first calendar data...');
-  return res.send(await generator);
+  const data = await generateNewCalendarData(times);
+  cacheData.put(cacheKey, data);
+  return res.send(data);
 }
