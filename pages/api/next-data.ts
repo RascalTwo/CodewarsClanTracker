565f066263c5cc7a3a1b14aa373c90284254a549
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-22 13:58:18 -0400

    Update timer to 6 hours

--- pages/api/next-data.ts
+++ pages/api/next-data.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import schedule from 'node-schedule';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const job = schedule.scheduleJob('next data', '0 */3 * * *', () => undefined);
+  const job = schedule.scheduleJob('next data', '0 */6 * * *', () => undefined);
   const time = job.nextInvocation().getTime();
   job.cancel();
   return res.status(200).send(time);
